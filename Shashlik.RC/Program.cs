a8107ea9cdaf825f7c0029e2ef4c815688372142
gucao <280780363@qq.com>
2020-12-08 14:34:27 +0800

    update README.md

--- Shashlik.RC/Program.cs
+++ Shashlik.RC/Program.cs
@@ -18,7 +18,7 @@ namespace Shashlik.RC
                 .ConfigureAppConfiguration(r =>
                 {
                     var file = new FileInfo("./data/appsettings.yaml").FullName;
-                    r.AddYamlFile(file);
+                    r.AddYamlFile(file,true);
                 })
                 .ConfigureWebHostDefaults((webBuilder) =>
                 {
