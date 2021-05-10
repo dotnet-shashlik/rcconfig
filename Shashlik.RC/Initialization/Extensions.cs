060afcab4da9165ae8471b349e6bcaba407f4d95
gucao <280780363@qq.com>
2021-05-10 15:36:32 +0800

    improve Initialization

--- Shashlik.RC/Initialization/Extensions.cs
+++ Shashlik.RC/Initialization/Extensions.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Data;
 using System.Linq;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Shashlik.Kernel;
 using Shashlik.RC.Common;
 using Shashlik.RC.Services.Identity;
@@ -11,29 +12,36 @@ namespace Shashlik.RC.Initialization
 {
     public static class Extensions
     {
-        private static void CreateRole(RoleService roleService, string role)
+        private static void InitRole(RoleService roleService, string role, ILogger logger)
         {
             var adminRole = roleService.FindByNameAsync(role).GetAwaiter().GetResult();
             if (adminRole is null)
             {
-                var res = roleService.CreateAsync(new IdentityRole<int>
+                try
                 {
-                    Name = role
-                }).GetAwaiter().GetResult();
-                if (!res.Succeeded && res.Errors.All(r => r.Code != "DuplicateRoleName"))
-                    throw new InvalidOperationException($"create role \"{role}\" failed, detail: {res}");
+                    var res = roleService.CreateAsync(new IdentityRole<int>
+                    {
+                        Name = role
+                    }).GetAwaiter().GetResult();
+                    if (!res.Succeeded && res.Errors.All(r => r.Code != "DuplicateRoleName"))
+                        throw new InvalidOperationException($"create role \"{role}\" failed, detail: {res}");
+                }
+                catch (Exception e)
+                {
+                    logger.LogWarning(e, $"create role \"{role}\" occurred error");
+                }
             }
         }
-
 
         public static IKernelServiceProvider InitRoleAndAdminUser(this IKernelServiceProvider serviceProvider)
         {
             using var scope = serviceProvider.CreateScope();
 
+            var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger("Initialization");
             using var userService = scope.ServiceProvider.GetRequiredService<UserService>();
             using var roleService = scope.ServiceProvider.GetRequiredService<RoleService>();
-            CreateRole(roleService, Constants.Roles.Admin);
-            CreateRole(roleService, Constants.Roles.User);
+            InitRole(roleService, Constants.Roles.Admin, logger);
+            InitRole(roleService, Constants.Roles.User, logger);
 
             var user = userService.FindByNameAsync(SystemEnvironmentUtils.AdminUser).GetAwaiter().GetResult();
             if (user is null)
@@ -43,11 +51,17 @@ namespace Shashlik.RC.Initialization
                     UserName = SystemEnvironmentUtils.AdminUser
                 };
 
-
-                var res = userService.CreateAsync(user, SystemEnvironmentUtils.AdminPassword).GetAwaiter().GetResult();
-                if (!res.Succeeded && res.Errors.All(r => r.Code != "DuplicateUserName"))
-                    throw new InvalidOperationException($"create admin user failed, detail: {res}");
-                userService.AddToRoleAsync(user, Constants.Roles.Admin).GetAwaiter().GetResult();
+                try
+                {
+                    var res = userService.CreateAsync(user, SystemEnvironmentUtils.AdminPassword).GetAwaiter().GetResult();
+                    if (!res.Succeeded && res.Errors.All(r => r.Code != "DuplicateUserName"))
+                        throw new InvalidOperationException($"create admin user failed, detail: {res}");
+                    userService.AddToRoleAsync(user, Constants.Roles.Admin).GetAwaiter().GetResult();
+                }
+                catch (DbUpdateException e)
+                {
+                    logger.LogWarning(e, $"create admin user occurred error");
+                }
             }
 
             return serviceProvider;
