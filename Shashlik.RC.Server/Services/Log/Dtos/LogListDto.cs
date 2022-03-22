cc98b84a061d0ca4db22e748573e998384e24c9e
gucao <280780363@qq.com>
2022-03-22 21:04:24 +0800

    add log class to git

---
+++ Shashlik.RC.Server/Services/Log/Dtos/LogListDto.cs
@@ -0,0 +1,37 @@
+﻿#nullable disable
+using Shashlik.AutoMapper;
+using Shashlik.RC.Data.Entities;
+
+namespace Shashlik.RC.Server.Services.Log.Dtos
+{
+    public class LogListDto : IMapFrom<Logs>
+    {
+        public long Id { get; set; }
+
+        public long LogTime { get; set; }
+
+        /// <summary>
+        /// 操作类型
+        /// </summary>
+        public string LogType { get; set; }
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 日志概要信息
+        /// </summary>
+        public string Description { get; set; }
+
+        public int FileId { get; set; }
+        
+        public string FileName { get; set; }
+
+        public string ResourceId { get; set; }
+
+        public string BeforeContent { get; set; }
+
+        public string AfterContent { get; set; }
+    }
+}
---
+++ Shashlik.RC.Server/Services/Log/Inputs/SearchLogInput.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shashlik.RC.Server.Common;
+
+namespace Shashlik.RC.Server.Services.Log.Inputs
+{
+    public class SearchLogInput : PageInput
+    {
+        public int? FileId { get; set; }
+
+        [StringLength(255)] public string? FileName { get; set; }
+    }
+}
