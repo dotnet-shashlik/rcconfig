584a77a080192561dee50d1504e56012c489460f
gucao <280780363@qq.com>
2022-03-27 17:07:54 +0800

    no msg

--- Shashlik.RC.Client/InternalService.cs
+++ Shashlik.RC.Client/InternalService.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Shashlik.RC.Client;
 
+
+//TODO: 优化，需要释放IServiceProvider
 internal class InternalService
 {
     public static IServiceCollection Services { get; } = new ServiceCollection();
